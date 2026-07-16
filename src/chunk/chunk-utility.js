b37c4440e7cf31216f2d0d9e0e93cc4334a33bc5
arbaz-17 <126326894+arbaz-17@users.noreply.github.com>
2026-07-16 14:40:18 +0500

    Added Chunk Utility

---
+++ src/chunk/chunk-utility.js
@@ -0,0 +1,32 @@
+function chunk(values, size) {
+  if (typeof values !== "string" && !Array.isArray(values)) {
+    throw new TypeError("Input must be a string or an array.");
+  }
+
+  if (size < 1 || !Number.isInteger(size)) {
+    throw new RangeError("Size must be an integer greater than 0");
+  }
+  
+  const result = [];
+  let currentChunk = [];
+
+  for (const item of values) {
+    currentChunk.push(item);
+
+    if (currentChunk.length === size) {
+      result.push(currentChunk);
+      currentChunk = [];
+    }
+  }
+
+  if (currentChunk.length > 0) {
+    result.push(currentChunk);
+  }
+
+  return result;
+}
+
+let values = 6;
+let size = 2;
+
+console.log("Chunks: ", chunk(values, size));
